a1954a0ce3b5315f0ca0a1360e71374542c80504
Ilifilza Rusli <ilifilza@snappymob.com>
2022-01-11 23:10:38 +0800

    Implement edit store flow

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -13,7 +13,36 @@ exports.createStore = async (req, res) => {
   const store = await new Store(req.body).save();
   req.flash(
     'success',
-    `Successfully Created ${store.name}. Care to leave a review?`
+    `Successfully created ${store.name}. Care to leave a review?`
   );
   res.redirect(`/stores/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // Query all items in the database
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // Find the store by ID
+  const store = await Store.findOne({ _id: req.params.id });
+
+  // Render the edit store form so user can update the details
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // Find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  }).exec();
+
+  // Redirect them the store and show success message
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</ strong>. <a href="/stores/${store.slug}">View Store -></a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
 };
