d4cd1b3b7254cdb5b2f2fd54c2cdd6e9884ea630
Ilifilza Rusli <ilifilza@snappymob.com>
2022-01-21 00:00:04 +0800

    Add middleware for store image upload feature

--- models/Store.js
+++ models/Store.js
@@ -13,6 +13,7 @@ const storeSchema = new mongoose.Schema({
     coordinates: [{ type: Number, required: 'You must supply coordinates.' }],
     address: { type: String, required: 'You must supply an address.' },
   },
+  photo: String,
 });
 
 storeSchema.pre('save', function(next) {
